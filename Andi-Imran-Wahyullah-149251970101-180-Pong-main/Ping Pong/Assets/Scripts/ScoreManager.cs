21bfbac95018ca58ceb419509a46099a3c42a03f
PionStudio <107085546+PionStudio@users.noreply.github.com>
2022-06-10 16:42:22 +0700

    Adding UI & Game Manager

---
+++ Andi-Imran-Wahyullah-149251970101-180-Pong-main/Ping Pong/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] private Text gawangKiriText;
+    [SerializeField] private Text gawangKananText;
+
+    private int skorKiri;
+    private int skorKanan;
+
+    [SerializeField] private int maxScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gawangKananText.text = skorKanan.ToString();
+        gawangKiriText.text = skorKiri.ToString();
+
+        if ((skorKanan >= maxScore) || (skorKiri >= maxScore))
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
+    }
+
+    public void CetakGoal(bool kiriGawang)
+    {
+        if (kiriGawang)
+        {
+            skorKiri++;
+            return;
+        }
+
+        skorKanan++;
+    }
+}
